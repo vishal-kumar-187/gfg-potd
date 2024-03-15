0660713825d20c1b5155f1e1ffe84774d86b9eae
vishal-kumar-187 <136841271+vishal-kumar-187@users.noreply.github.com>
2024-03-15 22:30:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Linked List that is Sorted Alternatingly/linked-list-that-is-sorted-alternatingly.cpp
@@ -0,0 +1,129 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+/* Function to print linked list */
+
+
+void append(struct Node** headRef, struct Node** tailRef, int newData)
+{
+	struct Node* new_node = new Node(newData);
+	struct Node *last = *headRef;
+	if (*headRef == NULL)
+	{
+	    *headRef = new_node;
+	    *tailRef = *headRef;
+	    return;
+	}
+	
+	(*tailRef) -> next = new_node;
+	*tailRef = (*tailRef) -> next;
+}
+
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	while (head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+
+
+// } Driver Code Ends
+
+/*
+struct Node
+{
+	int data;
+	struct Node *next;
+	
+	Node(int x){
+	    data =x;
+	    next = NULL;
+	}
+};
+*/
+
+class Solution//---------->15/03/24
+{
+public:
+    // Function to merge two sorted linked lists
+    Node* merge(Node* a, Node* b) {
+        if (!a) return b;
+        if (!b) return a;
+        
+        Node* result = NULL;
+        
+        if (a->data <= b->data) {
+            result = a;
+            result->next = merge(a->next, b);
+        } else {
+            result = b;
+            result->next = merge(a, b->next);
+        }
+        
+        return result;
+    }
+    
+    // Function to sort the linked list
+    Node* mergeSort(Node* head) {
+        if (!head || !head->next) return head;
+        
+        Node* slow = head;
+        Node* fast = head->next;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        Node* mid = slow->next;
+        slow->next = NULL;
+        
+        return merge(mergeSort(head), mergeSort(mid));
+    }
+    
+    // your task is to complete this function
+    void sort(Node **head)
+    {
+        *head = mergeSort(*head);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    struct Node* head = NULL;
+	    struct Node* tail = NULL;
+	    int n, k;
+	    cin>>n;
+	    for(int i=0; i<n ;i++){
+	        cin>>k;
+	        append(&head, &tail, k);
+	    }
+	    Solution ob;
+	    ob.sort(&head);
+	    printList(head);
+	}
+	return 0;
+}
+
+// } Driver Code Ends
