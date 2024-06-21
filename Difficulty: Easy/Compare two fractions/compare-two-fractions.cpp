6b1c4d9871e56cc7d3df1599ba1d6f02ceef28a7
vishal-kumar-187 <136841271+vishal-kumar-187@users.noreply.github.com>
2024-06-21 20:50:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Compare two fractions/compare-two-fractions.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+
+#include <iostream>
+#include <regex>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string compareFrac(string s) {
+
+        // Code here
+        int i=0, n=s.length();
+        double a=0, b=0, c=0, d=0;
+        while(s[i]!='/'){
+            a=a*10+(s[i]-'0');
+            i++;
+        }
+        i++;
+        
+        while(s[i]!=','){
+            b=b*10+(s[i]-'0');
+            i++;
+        }
+        i+=2;
+        
+        while(s[i]!='/'){
+            c=c*10+(s[i]-'0');
+            i++;
+        }
+        i++;
+        
+        while(i<n){
+            d=d*10+(s[i]-'0');
+            i++;
+        }
+        
+        
+        string fraction1=to_string(int(a))+'/'+to_string(int(b));
+        string fraction2=to_string(int(c))+'/'+to_string(int(d));
+        
+        if(a/b==c/d) return "equal";
+        else if(a/b > c/d) return fraction1;
+        else return fraction2;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    Solution ob;
+    int t;
+    cin >> t;
+    cin.ignore();
+    for (int i = 0; i < t; i++) {
+
+        string str;
+        getline(cin, str);
+
+        cout << ob.compareFrac(str) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
