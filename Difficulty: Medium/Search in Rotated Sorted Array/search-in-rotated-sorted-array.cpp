3219347e3b20a17334d8cccd9fa770ffd848b891
vishal-kumar-187 <136841271+vishal-kumar-187@users.noreply.github.com>
2024-12-14 23:54:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -5,36 +5,37 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-   public:
-  // solution - 1 
-    // int search(vector<int>& arr, int key) {
-    //     for(int i=0; i<arr.size(); i++){
-    //         if(arr[i]==key)
-    //             return i;
-    //     }
-    //     return -1;
-    // }
-
-  // solution - 2    
+  public:
     int search(vector<int>& arr, int key) {
-        int s = 0, e=arr.size()-1;
-        while(s <= e){
-            int mid = (s+e)/2;
-            if(arr[mid]==key){
+        // complete the function here
+        
+        int n = arr.size();
+        int low = 0, high = n-1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            //first trim down the search space to remove duplicetes;
+            if (arr[mid] == key){
                 return mid;
             }
-            if(arr[s] <= arr[mid]){
-                if(key >= arr[s] && key < arr[mid]){
-                    e = mid-1;
-                }else{
-                    s = mid+1;
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+                low++, high--;
+                continue;
+            }
+            //search for the sorted portion
+            if(arr[low] <= arr[mid]){//left half is sorted
+                if(arr[low] <= key && key < arr[mid]){
+                    high = mid - 1;
                 }
-            }else{
-                if(key > arr[mid] && key <= arr[e]){
-                    s = mid+1;
-                }else{
-                    e = mid-1;
+                else
+                    low = mid + 1;
+            }
+            else{//right half is sorted
+                if(arr[mid] < key && key <= arr[high]){
+                    low = mid + 1;
                 }
+                else
+                    high = mid - 1;
             }
         }
         return -1;
@@ -59,6 +60,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
